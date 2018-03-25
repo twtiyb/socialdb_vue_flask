7d3092b9b96b278bbb631a8e5e2cdab877472c49
徐纯 <chunchun.xu@wwwarehouse.com>
2018-03-25 09:34:25 +0800

    aa

--- backend/conf/config.py
+++ backend/conf/config.py
@@ -2,4 +2,5 @@ class MongoDBConfig:
     g_server_ip = '127.0.0.1'	#mongodb数据库地址
     g_server_port = 32768		#数据库端口
     g_db_name = 'socialdb'  		#数据库名
+    error_line_file = "error_log"
     ALLOWED_EXTENSIONS = ['txt', 'csv'] #允许上传的类型
